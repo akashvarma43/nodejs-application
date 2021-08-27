69fef9843c5b503a91f6b9e8b052816ccf233e17
Christopher Gioia <christophergioia64@gmail.com>
2021-08-26 22:30:43 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+// we might have multiple express applications per node project
+
+const app = express();
+
+// route handler
+app.get('/', (req, res) => {
+  res.send({ hi: 'there' });
+});
+
+app.listen(5000);
